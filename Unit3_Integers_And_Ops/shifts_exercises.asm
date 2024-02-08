ac953ff979cc9ee3d025537786f0a5e22a3c3a67
rzessin23 <rzessin@stedwards.edu>
2024-02-08 21:01:26 +0000

    initial commit

--- Unit3_Integers_And_Ops/shifts_exercises.asm
+++ Unit3_Integers_And_Ops/shifts_exercises.asm
@@ -22,6 +22,48 @@ asm_main:
         pusha
 
 		; TODO: add code
+        ;mov eax, 10
+        ;shl eax, 3      ;shift to the 
+        ;call print_int
+
+        ;mov ebx, 2
+        ;shl eax, ebx
+        ;call print_int
+
+        ;mov eax, 080000045H
+        ;call print_int
+        ;call print_nl
+
+        ;mov eax, 23
+        ;shr eax, 2
+        ;call print_int
+
+        ;mov eax, 4
+        ;mov ebx, 2
+        ;call print_int
+        ;test eax, ebx
+        ;dump_regs 1
+
+        ;mov eax, 13
+        ;xor eax, eax
+        ;call print_int
+
+        ;mov eax, 018H
+        ;not eax
+
+       ; mov ebx, 1
+        ;shl ebx, 3
+        ;not ebx
+
+        ;mov eax, 15
+        ;or eax, ebx
+        ;call print_int
+
+        mov ebx, 00000FFFFH
+        mov eax, -1
+
+        and eax, ebx
+        dump_regs 1
 
         popa
         mov     eax, 0            ; return back to C
